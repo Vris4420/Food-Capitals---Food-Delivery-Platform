5edc49f1de91e7170b76c3ded8957e2f110b8644
Vrishank Sharma <sharma08462@gmail.com>
2024-04-26 22:24:16 +0530

    Sidebar styling completed

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,28 @@
-import './Sidebar.css'
+import "./Sidebar.css";
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBag, faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
-export function Sidebar(){
-    
+export function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <div>
+            <FontAwesomeIcon icon={faShoppingBag} />
+          </div>
+          <p>Add Items</p>
+        </div>
+        <div className="sidebar-option">
+          <FontAwesomeIcon icon={faCirclePlus} />
+
+          <p>List Items</p>
+        </div>
+        <div className="sidebar-option">
+          <FontAwesomeIcon icon={faCirclePlus} />
+          <p>Orders</p>
+        </div>
+      </div>
+    </div>
+  );
 }
