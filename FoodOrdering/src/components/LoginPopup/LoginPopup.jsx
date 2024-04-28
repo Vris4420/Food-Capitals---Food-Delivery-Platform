e9c84c043f3df7effa5192a60305a4f301f13916
Vrishank Sharma <sharma08462@gmail.com>
2024-04-28 15:34:56 +0530

    onLogin function added

--- FoodOrdering/src/components/LoginPopup/LoginPopup.jsx
+++ FoodOrdering/src/components/LoginPopup/LoginPopup.jsx
@@ -3,10 +3,11 @@ import "./LoginPopup.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { StoreContext } from "../../context/StoreContext";
+import axios from 'axios'
 
 export function LoginPopup({ setShowLogin }) {
 
-  const {url} = useContext(StoreContext)
+  const {url,setToken} = useContext(StoreContext)
 
   const [currState, setCurrState] = useState("Login");
   const [data,setData] = useState({
@@ -21,11 +22,29 @@ export function LoginPopup({ setShowLogin }) {
     setData(data => ({...data,[name]:value}))
   }
 
-  
+  async function onLogin(event){
+    event.preventDefault();
+    let newUrl = url;
+    if(currState === 'Login'){
+      newUrl += '/api/user/login'
+    }else{
+      newUrl += '/api/user/register'
+    }
+
+    const response = await axios.post(newUrl,data)
+
+    if (response.data.success) {
+      setToken(response.data.token);
+      localStorage.setItem("token",response.data.token);
+      setShowLogin(false)
+    }else{
+      alert(response.data.message)
+    }
+  }
 
   return (
     <div className="login-popup">
-      <form className="login-popup-container">
+      <form onSubmit={onLogin} className="login-popup-container">
         <div className="login-popup-title">
           <h2>{currState}</h2>
           <div className="Cross-icon" onClick={() => setShowLogin(false)}>
@@ -41,7 +60,7 @@ export function LoginPopup({ setShowLogin }) {
           <input name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder="Your Email Id" required />
           <input name="password" onChange={onChangeHandler} value={data.password} type="password" placeholder="Your Password" required />
         </div>
-        <button>{currState === "Sign up" ? "Create Account" : "Login"}</button>
+        <button type="submit">{currState === "Sign up" ? "Create Account" : "Login"}</button>
         <div className="login-popup-condition">
           <input type="checkbox" required />
           <p>By continuing, I agree to the terms of use and privacy policy</p>
--- FoodOrdering/src/context/StoreContext.jsx
+++ FoodOrdering/src/context/StoreContext.jsx
@@ -6,6 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url = 'http://localhost:4000'
+  const [token,setToken] = useState("");
 
   function addToCart(itemId) {
     if (!cartItems[itemId]) {
@@ -48,7 +49,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
-    url
+    url,
+    token,
+    setToken,
   };
 
   return (
